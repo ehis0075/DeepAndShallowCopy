a7afcb54b2ddf9b81d32014c9e9bfe54abab4d8d
ehis0075 <ehisjude420@gmail.com>
2023-07-27 15:38:30 +0100

    added numbering on the unit test cases

--- src/test/java/com/demo/demo/model/PersonTest.java
+++ src/test/java/com/demo/demo/model/PersonTest.java
@@ -17,24 +17,24 @@ public class PersonTest {
         hobbies.add("Reading");
         hobbies.add("Swimming");
 
-        // Create a Person object
+        //1 Create a Person object
         Person originalPerson = new Person("Alice", 85, hobbies);
 
-        // Clone the Person object
+        //2 Clone the Person object
         Person clonedPerson = originalPerson.clone();
 
-        // Check that the clonedPerson is not the same reference as the originalPerson (shallow copy)
+        //3 Check that the clonedPerson is not the same reference as the originalPerson (shallow copy)
         Assertions.assertNotSame(originalPerson, clonedPerson);
 
-        // Check that the clonedPerson has the same values as the originalPerson (shallow copy)
+        //4 Check that the clonedPerson has the same values as the originalPerson (shallow copy)
         Assertions.assertEquals(originalPerson.getName(), clonedPerson.getName());
         Assertions.assertEquals(originalPerson.getScore(), clonedPerson.getScore());
 
 
-        // Modify the hobbies list in the originalPerson
+        //5 Modify the hobbies list in the originalPerson
         originalPerson.getHobbies().add("Hiking");
 
-        // Check that the hobbies list in the clonedPerson is not affected by the modification (deep copy)
+        //6 Check that the hobbies list in the clonedPerson is not affected by the modification (deep copy)
         Assertions.assertNotEquals(originalPerson.getHobbies(), clonedPerson.getHobbies());
     }
 
